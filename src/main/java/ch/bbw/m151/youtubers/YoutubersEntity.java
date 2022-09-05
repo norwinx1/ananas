2adc0b4f683eeac54c1249554aa647e431171d48
Norwin Schäfer <norwin270@gmail.com>
2022-09-05 15:46:09 +0200

    finished queries

--- src/main/java/ch/bbw/m151/youtubers/YoutubersEntity.java
+++ src/main/java/ch/bbw/m151/youtubers/YoutubersEntity.java
@@ -26,4 +26,83 @@ public class YoutubersEntity {
 
     private Long avgComments;
 
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getYoutubeUrl() {
+        return youtubeUrl;
+    }
+
+    public void setYoutubeUrl(String youtubeUrl) {
+        this.youtubeUrl = youtubeUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Long getAmountSubscribers() {
+        return amountSubscribers;
+    }
+
+    public void setAmountSubscribers(Long amountSubscribers) {
+        this.amountSubscribers = amountSubscribers;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public Long getAvgViews() {
+        return avgViews;
+    }
+
+    public void setAvgViews(Long avgViews) {
+        this.avgViews = avgViews;
+    }
+
+    public Long getAvgLikes() {
+        return avgLikes;
+    }
+
+    public void setAvgLikes(Long avgLikes) {
+        this.avgLikes = avgLikes;
+    }
+
+    public Long getAvgComments() {
+        return avgComments;
+    }
+
+    public void setAvgComments(Long avgComments) {
+        this.avgComments = avgComments;
+    }
 }
--- src/test/java/ch/bbw/m151/youtubers/YoutubersApplicationTests.java
+++ src/test/java/ch/bbw/m151/youtubers/YoutubersApplicationTests.java
@@ -1,13 +1,44 @@
 package ch.bbw.m151.youtubers;
 
+import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @SpringBootTest
-class YoutubersApplicationTests {
+class YoutubersApplicationTests implements WithAssertions {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private YoutubersRepository youtubersRepository;
 
+    @Test
+    void testCount() {
+        assertThat(youtubersRepository.count()).isEqualTo(1000);
+    }
+
+    @Test
+    void test100MilChannels() {
+        assertThat(youtubersRepository.findAllByAmountSubscribersGreaterThan(100000000L).size()).isEqualTo(3);
+    }
+
+    @Test
+    void testSportsCategory() {
+        List<YoutubersEntity> list = youtubersRepository.findAllByCategoryEqualsIgnoreCaseOrderByUsername("Sports");
+        assertThat(list.size()).isEqualTo(8);
+    }
+
+    @Test
+    void testUsernameWithA() {
+        List<YoutubersEntity> list = youtubersRepository.findAllByUsernameContainingOrderByUsername("a", Pageable.ofSize(10));
+        System.out.println(list);
+    }
+
+    @Test
+    void testDistinctCountry() {
+        List<String> list = youtubersRepository.distinctCountries();
+        assertThat(list.size()).isEqualTo(28);
+    }
 }
